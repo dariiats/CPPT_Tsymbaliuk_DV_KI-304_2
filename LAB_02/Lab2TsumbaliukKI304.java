042711333a67145c5a1dda8c5efd62861d55cfa5
daria <daryiats@gmail.com>
2025-09-17 18:24:04 +0300

    Додала коментарі

--- LAB_02/Lab2TsumbaliukKI304.java
+++ LAB_02/Lab2TsumbaliukKI304.java
@@ -2,50 +2,84 @@ package LAB_02;
 
 import java.io.FileNotFoundException;
 
+/**
+ * Driver class to test and demonstrate the functionality of the Shlupka class.
+ * It creates boats with default and custom configurations, performs various actions,
+ * and logs all activity to the corresponding log files.
+ */
 public class Lab2TsumbaliukKI304 {
+
+    /**
+     * Main method to run the demonstration of the Shlupka class.
+     *
+     * @param args command-line arguments (not used)
+     * @throws FileNotFoundException if log files cannot be created
+     */
     public static void main(String[] args) throws FileNotFoundException {
+        // Create boat with default configuration
         Shlupka boat1 = new Shlupka();
 
+        // Display initial boat information
         boat1.boatInfo();
 
+        // Load 1 passenger
         boat1.loadPassangers(1);
 
+        // Attempt to unload 2 passengers (should show a warning)
         boat1.unloadPassangers(2);
 
+        // Board all remaining available passengers
         boat1.boardAllPassengers();
 
+        // Show remaining capacity
         boat1.passengerCapacityLeft();
 
+        // Show current movement status
         boat1.status();
 
+        // Start rowing
         boat1.row();
 
+        // Stop the boat
         boat1.stop();
 
+        // Show updated boat information
         boat1.boatInfo();
 
+        // Close log file
         boat1.dispose();
 
-        Shlupka boat2 = new Shlupka(4, "plastic", 8, 4, 8, "ShlupkaLog2.txt");
+        // Create boat with custom configuration and log file
+        Shlupka boat2 = new Shlupka(5, "plastic", 8, 4, 8, "ShlupkaLog2.txt");
 
+        // Display initial boat information
         boat2.boatInfo();
 
+        // Load 5 passengers
         boat2.loadPassangers(5);
 
+        // Unload 2 passengers
         boat2.unloadPassangers(2);
 
+        // Board all remaining available passengers
         boat2.boardAllPassengers();
 
+        // Show remaining capacity
         boat2.passengerCapacityLeft();
 
+        // Show current movement status
         boat2.status();
 
+        // Start rowing
         boat2.row();
 
+        // Stop the boat
         boat2.stop();
 
+        // Show updated boat information
         boat2.boatInfo();
 
+        // Close log file
         boat2.dispose();
     }
 }
--- LAB_02/Shlupka.java
+++ LAB_02/Shlupka.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+/**
+ * Class <code>Shlupka</code> models a rowing boat with oars, hull, and seats.
+ * It allows managing passengers, boat movement, and logs all actions to a file.
+ */
 public class Shlupka {
 
     private Oars oars;
@@ -13,6 +17,12 @@ public class Shlupka {
     private boolean move;
     PrintWriter fout;
 
+    /**
+     * Default constructor. Creates a boat with default configuration:
+     * 2 oars, wooden hull 6x2 meters, 6 seats, and empty log file "ShlupkaLog.txt".
+     * 
+     * @throws FileNotFoundException if the log file cannot be created
+     */
     public Shlupka() throws FileNotFoundException {
         this.oars = new Oars();
         this.hull = new Hull();
@@ -22,6 +32,17 @@ public class Shlupka {
         fout = new PrintWriter(new File("ShlupkaLog.txt"));
     }
 
+    /**
+     * Constructor with parameters to create a boat with specific configuration.
+     *
+     * @param countOars  number of oars
+     * @param material   material of the hull
+     * @param length     hull length in meters
+     * @param width      hull width in meters
+     * @param countSeats number of seats
+     * @param nameFile   name of the log file
+     * @throws FileNotFoundException if the log file cannot be created
+     */
     public Shlupka(int countOars, String material, double length, double width, int countSeats, String nameFile)
             throws FileNotFoundException {
         oars = new Oars(countOars);
@@ -32,136 +53,150 @@ public class Shlupka {
         fout = new PrintWriter(new File(nameFile));
     }
 
+    /**
+     * Loads a specified number of passengers onto the boat.
+     * Logs the action and warns if there are not enough seats.
+     *
+     * @param count number of passengers to load
+     */
     public void loadPassangers(int count) {
         if ((count + passanger) <= seats.countSeats) {
-            passanger = passanger + count;
+            passanger += count;
             fout.println("Load passangers. Boat has: " + passanger + " passangers");
-        }
-
-        else {
-            fout.println("Too much passangers. Maximum passangers on boat is : " + seats.countSeats + ". Boat has: "
-                    + passanger + " passangers");
+        } else {
+            fout.println("Too many passengers. Maximum seats: " + seats.countSeats + ". Current: " + passanger);
         }
     }
 
+    /**
+     * Loads all available passengers onto the boat.
+     * Logs how many passengers were boarded.
+     */
     public void boardAllPassengers() {
         int availableSeats = seats.getCountSeats() - passanger;
-        
         if (availableSeats > 0) {
-            passanger = passanger + availableSeats;
-            fout.println("All available passengers boarded: " + availableSeats + " passengers. Total now: " + passanger);
-        } 
-        
-        else {
+            passanger += availableSeats;
+            fout.println("All available passengers boarded: " + availableSeats + ". Total now: " + passanger);
+        } else {
             fout.println("No available seats to board passengers. Total passengers: " + passanger);
         }
     }
 
+    /**
+     * Unloads a specified number of passengers from the boat.
+     * Logs the action and warns if the boat is empty or count is invalid.
+     *
+     * @param count number of passengers to unload
+     */
     public void unloadPassangers(int count) {
-        if (passanger > 0 && passanger >= count) {
-            passanger = passanger - count;
+        if (passanger >= count) {
+            passanger -= count;
             fout.println("Unload passangers. Boat has: " + passanger + " passangers");
-        }
-
-        else if (passanger == 0) {
-            fout.println("Impossible unload passanger. Boats is empty!");
-        }
-
-        else {
-            fout.println("Impossible unload this passangers' count. Boat has: " + passanger + " passangers");
+        } else if (passanger == 0) {
+            fout.println("Impossible unload passanger. Boat is empty!");
+        } else {
+            fout.println("Impossible unload this count. Boat has: " + passanger + " passangers");
         }
     }
 
+    /**
+     * Starts moving the boat if there are passengers and oars.
+     * Logs the action or reason for failure.
+     */
     public void row() {
         if (!move && passanger > 0 && oars.getCountOars() > 0) {
             move = true;
-            fout.println("Boat start to move with " + passanger + " passangers");
-        }
-
-        else if (move) {
-            fout.println("Boat is aleady moving!");
-        }
-
-        else {
-            fout.println("Impossible to move! Boat doesn't have passangers or oars");
+            fout.println("Boat starts moving with " + passanger + " passangers");
+        } else if (move) {
+            fout.println("Boat is already moving!");
+        } else {
+            fout.println("Impossible to move! Boat doesn't have passengers or oars");
         }
     }
 
+    /**
+     * Stops the boat if it is moving.
+     * Logs the action or reason for failure.
+     */
     public void stop() {
-        if (move && passanger > 0 && oars.getCountOars() > 0) {
+        if (move) {
             move = false;
-            fout.println("Boat stop to move with " + passanger + " passangers");
-        }
-
-        else if (!move) {
-            fout.println("Boat already stop!");
-        }
-
-        else {
-            fout.println("Impossible to stop! Boat doesn't have passangers or oars");
+            fout.println("Boat stops moving with " + passanger + " passangers");
+        } else {
+            fout.println("Boat already stopped!");
         }
     }
 
+    /**
+     * Prints the current movement status to the log file.
+     */
     public void status() {
-        fout.println("Status: " + (move == false ? "doesn't moving" : "moving"));
+        fout.println("Status: " + (move ? "moving" : "not moving"));
     }
 
+    /**
+     * Prints the number of free seats available for passengers.
+     */
     public void passengerCapacityLeft() {
-        int left = (seats.getCountSeats() - passanger) >= 0 ? (seats.getCountSeats() - passanger) : 0;
+        int left = seats.getCountSeats() - passanger;
+        left = left >= 0 ? left : 0;
         fout.println("Available passenger seats: " + left);
     }
 
+    /**
+     * Prints full information about the boat's configuration and current state.
+     */
     public void boatInfo() {
         fout.println("Number of oars: " + oars.getCountOars());
-        fout.println("Boat's material: " + hull.material);
-        fout.println("Boat's lenght: " + hull.lengthMeters);
-        fout.println("Boat's width: " + hull.widthMeters);
+        fout.println("Boat material: " + hull.material);
+        fout.println("Hull length: " + hull.lengthMeters);
+        fout.println("Hull width: " + hull.widthMeters);
         fout.println("Seats: " + seats.countSeats);
-        fout.println("Passangers: " + passanger);
-        fout.println("Status: " + (move == false ? "doesn't moving" : "moving"));
+        fout.println("Passengers: " + passanger);
+        fout.println("Status: " + (move ? "moving" : "not moving"));
     }
 
+    /**
+     * Clears the log file and starts a new session.
+     *
+     * @throws FileNotFoundException if the log file cannot be created
+     */
     public void clearLog() throws FileNotFoundException {
         fout.close();
         fout = new PrintWriter(new File("ShlupkaLog.txt"));
         fout.println("----- New session -----");
     }
 
+    /**
+     * Closes the log file and releases resources.
+     */
     public void dispose() {
         fout.close();
     }
 
+    /**
+     * Inner class representing oars of the boat.
+     */
     class Oars {
         private int countOars;
 
-        private Oars(int countOars) {
-            this.countOars = countOars;
-        }
+        private Oars() { countOars = 2; }
 
-        private Oars() {
-            countOars = 2;
-        }
+        private Oars(int countOars) { this.countOars = countOars; }
 
-        private int getCountOars() {
-            return countOars;
-        }
+        private int getCountOars() { return countOars; }
 
-        private void setCountOars(int countOars) {
-            this.countOars = countOars;
-        }
-
-        private void addOars() {
-            countOars++;
-        }
+        private void addOars() { countOars++; }
 
         private void removeOars() {
-            if (countOars > 0)
-                countOars--;
-            else
-                System.out.println("You already haven't oars!");
+            if (countOars > 0) countOars--;
+            else System.out.println("No oars to remove!");
         }
     }
 
+    /**
+     * Inner class representing the hull of the boat.
+     */
     class Hull {
         private String material;
         private double lengthMeters;
@@ -179,59 +214,32 @@ public class Shlupka {
             this.widthMeters = widthMeters;
         }
 
-        private String getMaterial() {
-            return material;
-        }
+        private String getMaterial() { return material; }
 
-        private double getLengthMeters() {
-            return lengthMeters;
-        }
+        private void setMaterial(String material) { this.material = material; }
 
-        private double getWidthMeters() {
-            return widthMeters;
-        }
+        private double getLengthMeters() { return lengthMeters; }
 
-        private void setMaterial(String material) {
-            this.material = material;
-        }
-
-        private void setLengthMeters(double lengthMeters) {
-            this.lengthMeters = lengthMeters;
-        }
-
-        private void setWidthMeters(double widthMeters) {
-            this.widthMeters = widthMeters;
-        }
+        private double getWidthMeters() { return widthMeters; }
     }
 
+    /**
+     * Inner class representing seats of the boat.
+     */
     class Seats {
         private int countSeats;
 
-        private Seats() {
-            countSeats = 6;
-        }
+        private Seats() { countSeats = 6; }
 
-        private Seats(int countSeats) {
-            this.countSeats = countSeats;
-        }
+        private Seats(int countSeats) { this.countSeats = countSeats; }
 
-        private int getCountSeats() {
-            return countSeats;
-        }
+        private int getCountSeats() { return countSeats; }
 
-        private void setCountSeats(int countSeats) {
-            this.countSeats = countSeats;
-        }
-
-        private void addSeats() {
-            countSeats++;
-        }
+        private void addSeats() { countSeats++; }
 
         private void removeSeats() {
-            if (countSeats > 0)
-                countSeats--;
-            else
-                System.out.println("You already haven't seats!");
+            if (countSeats > 0) countSeats--;
+            else System.out.println("No seats to remove!");
         }
     }
 }
